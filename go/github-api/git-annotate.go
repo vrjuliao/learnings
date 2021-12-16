47e85ee4b26b35ac787f61be4553c5e3a98caca1
Vinicius Julião R <40807325+vrjuliao@users.noreply.github.com>
2021-12-16 17:17:50 -0300

    This is title (#1)
    * test merging
    
    * adding new commit title
    
    new commit body
    
    * backout commit
    
    backout commit body
    
    #add

---
+++ go/github-api/git-annotate.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/libgit2/git2go/v33"
+import "fmt"
+import "C"
+
+func main () {
+	repo, err := git.OpenRepository("/home/vrjuliao/workfolder/learnings/")
+	if err != nil {
+		fmt.Println("open repo problem")
+		fmt.Println(err)
+	}
+	// repo.LookupBranch("master", 0)
+	br, err := repo.LookupBranch("master", git.BranchAll)
+	if err != nil {
+		fmt.Println("Branch getting problem")
+		fmt.Println(err)
+	}
+	fmt.Println(br.Name())
+}
+
+func git_log() {
+	repo, err := git.OpenRepository("/home/vrjuliao/workfolder/learnings/")
+	var oid *Oid
+	var commit *Commit
+	walker := repo.Walk()
+	for walker.Next(oid) == nil {
+
+	}
+}
