a03b5f59ca4b9c5d3bfb097ee0a3c63dd2dd42d0
vrjuliao <vrjuliao@hotmail.com>
2021-12-20 18:53:18 -0300

    example(git log): git log example in GO

---
+++ go/github-api/git-log.go
@@ -0,0 +1,58 @@
+package main
+
+import "github.com/libgit2/git2go/v33"
+import "fmt"
+
+func main () {
+	repo, _ := git.OpenRepository("/home/vrjuliao/cyral/devops/api-server/")
+	git_log(repo)
+}
+
+func git_log(repo *git.Repository) {
+	// git default object
+	oid := new(git.Oid)
+	
+	// options of `git --describe` command
+	describeOpt,_ := git.DefaultDescribeOptions()
+	describeOpt.Strategy = git.DescribeTags
+	describeFmt, _:= git.DefaultDescribeFormatOptions()
+	
+	// initialize commit iterator
+	walker, _ := repo.Walk()
+	walker.PushHead()
+	
+	// getting next commit from HEAD
+	var nxt = walker.Next(oid)
+	if nxt != nil {
+		fmt.Println("Err:", nxt)
+	}
+	var count = 0
+
+	for nxt  == nil {
+		count = count + 1
+		commit, _ := repo.LookupCommit(oid)
+		if count > 4 {
+			break
+		}
+		// get commit Signature
+		author := commit.Author()
+		fmt.Println("Author:", author.Name)
+		
+		fmt.Println("Message:", commit.Message())
+		
+		// get commit tag
+		describeResult, err := commit.Describe(&describeOpt)
+		if err != nil {
+			fmt.Println("Descbribe err:", err)
+		} else {
+			tag, err := describeResult.Format(&describeFmt)
+			if err != nil {
+				fmt.Println("Described result error:", err)
+			} else {
+				fmt.Println("Tag:", tag)
+			}
+		}
+		fmt.Println("-------------------")
+		nxt = walker.Next(oid)
+	}
+}
